8c0bdee92f9b0bc4aa9ffe21d6a33f884f0a87a6
SHaLLaW <105590214+shallaw1@users.noreply.github.com>
2024-10-16 23:17:26 +0300

    Add repeat function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    repeat = '';
+    if(num >= 0) {
+        for (let i = 0; i < num; i++){
+            repeat += string;
+        }
+    } else {
+        repeat = "ERROR";
+    }
+    return repeat;
 };
 
 // Do not edit below this line
