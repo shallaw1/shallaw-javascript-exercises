db61599cff2c7fc6e6e6c26a54836bc780e5ac89
SHaLLaW-MsI <105590214+shallaw1@users.noreply.github.com>
2024-10-27 14:31:37 +0300

    add all numbers between both input

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // We need to define the max value bwtween the two parameters.
+    maxValue = Math.max(num1, num2);
+    let sumAllNum=0;
 
+    // negative integers we could not be accespted.
+    if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    
+    // we add between both input if and only if both are integer
+    }else if (Number.isInteger(num1) && Number.isInteger(num2)) {
+        for(i=1; i <= maxValue; i++){
+            sumAllNum += i;
+        }
+    } else {
+        return "ERROR";
+    }
+    return sumAllNum;
 };
 
 // Do not edit below this line
