437b2220804704c8e29cb12778ba1258086da6e6
SHaLLaW-MsI <105590214+shallaw1@users.noreply.github.com>
2024-10-29 13:53:19 +0300

    temperature conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+let y = 32;
+const convertToCelsius = function(temperature) {
+  if (temperature === y) return 0;
+  let result = ((temperature - y) * 5)/9;
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  if (temperature === 0) return 32;
+  let result = ((temperature * 9)/5) +32;
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
