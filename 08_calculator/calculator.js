490845f85515e9f8995631192c804a6ad9a62e4d
SHaLLaW-MsI <105590214+shallaw1@users.noreply.github.com>
2024-10-29 10:55:23 +0300

    factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num1) {
+  let sumOfNumber = 0;
+  if (num1.length === 0){
+    return 0;
+  }else{
+	for(let i = 0; i < num1.length; i++ ){
+    sumOfNumber += num1[i];
+  }
+  return sumOfNumber;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(num1) {
+  let multiplyNumber = 1;
+  for(let i = 0; i < num1.length; i++){
+    multiplyNumber *=num1[i];
+  }
+  return multiplyNumber;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base**exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+  if(factorial === 0){
+    return 1;
+  }else{
+    let result = 1;
+    for(let i = 1; i <= factorial; i++){
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
