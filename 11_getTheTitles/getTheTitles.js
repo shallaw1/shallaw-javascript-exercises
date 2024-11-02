b2ba4e2e5c1659a9b4c6cd0f2213f202b5ae09be
SHaLLaW <105590214+shallaw1@users.noreply.github.com>
2024-11-02 16:02:44 +0300

    add function to loop through an array of object return a new array with the title

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(book) {
+    bookTitle = [];
+    for (let i = 0; i < book.length; i++){
+        bookTitle.push(book[i].title);
+    }
+    return bookTitle;
 };
 
 // Do not edit below this line
